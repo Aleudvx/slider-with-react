55e2d88482989a35620fc38c4d07cbbe91a2110b
Aleudvx <rahmetelalihan@gmail.com>
2022-05-06 18:11:49 +0600

    Creating react slider

---
+++ src/Slider/Slider.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react"
+import "./dummyData.js"
+export default class Slider extends Component{
+    render(){
+        return(
+            <div className="Slider">
+                This is Slider
+            </div>
+        )
+    }
+}
---
+++ src/Slider/dummyData.js
@@ -0,0 +1,20 @@
+const slideData =[
+    {
+        id: 0,
+        headline: "New Apparel",
+        buttonText: "Shop now",
+        image: "https://images.unsplash.com/photo-1501870190084-cdf29f15ef87?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+    },
+    {
+        id: 1,
+        title: "In the Wilerness",
+        buttonText: "Book Travel",
+        image: "https://images.unsplash.com/photo-1436491865332-7a61a109cc05?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1174&q=80"
+        },
+    {
+        id:2,
+        title: "For Your Current Mood",
+        buttonText: "Listen",
+        image: "https://images.unsplash.com/photo-1470225620780-dba8ba36b745?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+    }
+]
